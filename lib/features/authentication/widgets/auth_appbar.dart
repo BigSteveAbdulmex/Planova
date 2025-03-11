73f2273e252f9941ef6490a3afe7b9e526855de9
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-03-11 20:57:13 +0100

    Custom appbar for both login and signup auth pages

--- lib/features/authentication/widgets/auth_appbar.dart
+++ lib/features/authentication/widgets/auth_appbar.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/material.dart';
 
-class AuthAppbar extends StatelessWidget {
-  const AuthAppbar({
-    super.key,
-    this.title,
-    this.actions,
-    this.leadingIcon,
-    this.leadingOnPressed,
-    this.showBackArrow = true,
-  });
+import 'package:planova/features/authentication/widgets/custom_curved_edge.dart';
 
-  final Widget? title;
-  final bool showBackArrow;
-  final IconData? leadingIcon;
-  final List<Widget>? actions;
-  final VoidCallback? leadingOnPressed;
+class CustomAuthAppbar extends StatelessWidget {
+  const CustomAuthAppbar({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: 16.0,
-      ),
-      child: AppBar(
-        automaticallyImplyLeading: false,
-        leading: const Icon(Icons.arrow_back),
+    return ClipPath(
+      clipper: CustomCurvedEdge(),
+      child: Container(
+        color: Colors.lightBlue,
+        padding: EdgeInsets.all(0),
+        child: SizedBox(
+          height: 250,
+          child: Stack(
+            children: [
+              Positioned(
+                top: -130,
+                right: -250,
+                child: Container(
+                  width: 400,
+                  height: 250,
+                  padding: const EdgeInsets.all(0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(300),
+                    color: Colors.white.withValues(alpha: 0.1),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 50,
+                right: -300,
+                child: Container(
+                  width: 400,
+                  height: 300,
+                  padding: const EdgeInsets.all(0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(300),
+                    color: Colors.white.withValues(alpha: 0.1),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
