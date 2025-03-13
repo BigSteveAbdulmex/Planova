6c60d8b4555b07d9334a4c43925663236e78ef10
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-03-13 23:47:44 +0100

    Still editing the login screen

--- lib/features/authentication/pages/login_page.dart
+++ lib/features/authentication/pages/login_page.dart
@@ -14,7 +14,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xFFFFFFF0),
       body: SingleChildScrollView(
         child: Column(
           children: [
