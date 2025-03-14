53c840644c23d5acbbed404a95e83f5bf3949ebc
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-03-14 22:48:29 +0100

    Started working on the sign up page

--- lib/features/authentication/pages/signup_page.dart
+++ lib/features/authentication/pages/signup_page.dart
@@ -11,6 +11,14 @@ class SignupPage extends StatelessWidget {
         child: Column(
           children: [
             CustomAuthAppbar(),
+            Text(
+              "Create an account",
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 21,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
           ],
         ),
       ),
